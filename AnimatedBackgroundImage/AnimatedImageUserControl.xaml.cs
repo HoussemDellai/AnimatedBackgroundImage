b6325d012c2adede2278bcf153e7259e0ee3e33c
HoussemDellai <houssem.dellai@live.com>
2014-01-31 08:17:34 -0800

    First commit.
    Minimum required files.

---
+++ AnimatedBackgroundImage/AnimatedImageUserControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+// Houssem Dellai
+// http://houssemdellai.net
+// houssem.dellai@live.com
+// @HoussemDellai
+
+namespace AnimatedBackgroundImage
+{
+	public sealed partial class AnimatedImageUserControl
+	{
+		public AnimatedImageUserControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
